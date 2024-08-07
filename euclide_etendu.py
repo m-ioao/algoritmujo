58e5eed6b604be8eb7e1f2d02622845b488dafef
GLC <61211757+m-ioao@users.noreply.github.com>
2024-08-07 19:10:33 +0200

    Update euclide_etendu.py

--- euclide_etendu.py
+++ euclide_etendu.py
@@ -1,4 +1,24 @@
-def euclideetendu(a,b):
+import numpy as np
+from numpy.linalg import inv
+
+
+#%% version matricielle
+# Renvoie un triplet (pgcd, u, v) où u, v coeff pour relation de bézout
+
+def euclide_etendu_matriciel(a,b):
+    M = np.identity(2)
+    while b != 0:
+        q = a//b
+        M = np.dot(M, np.array([[q, 1],
+                                [1, 0]]))
+        a, b = b, a%b
+    M = inv(M)
+    return (a, round(M[0][0]), round(M[0][1]))
+
+
+
+#%% version standard
+def euclide_etendu(a,b):
     u_i, v_i = 1, 0
     u_j, v_j = 0, 1
     
@@ -13,6 +33,4 @@ def euclideetendu(a,b):
         u_j, v_j = u_k, v_k
 
     return u_k, v_k
-
-print(euclideetendu(85,27))
 
