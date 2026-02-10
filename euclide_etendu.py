40881651b07ae9e20b8e431f46640dd4b04d14c6
GLC <61211757+m-ioao@users.noreply.github.com>
2026-02-10 11:20:47 +0000

    Update euclide_etendu.py
    ajouts et correction erreurs

--- euclide_etendu.py
+++ euclide_etendu.py
@@ -19,18 +19,40 @@ def euclide_etendu_matriciel(a,b):
 
 #%% version standard
 def euclide_etendu(a,b):
-    u_i, v_i = 1, 0
-    u_j, v_j = 0, 1
+    u_i, v_i = 1, 0                         # rang i correspondra à "n-2"
+    u_j, v_j = 0, 1                         # rang j correspondra à "n-1"
     
-    while b != 1:
-        q, r = a//b , a%b
+    while b != 0:                           # arrêt pour reste nul
+        quotient, reste = a//b , a%b
         
-        u_k = u_i - u_j*q
-        v_k = v_i - v_j*q
+        u_k = u_i - u_j * quotient          # rang k correspondra à "n"
+        v_k = v_i - v_j * quotient
         
-        a, b = b, r
+        a, b = b, reste
         u_i, v_i = u_j, v_j
         u_j, v_j = u_k, v_k
+                                            # on doit renvoyer le rang i, vu les réaffectations
+    return (u_i, v_i), a                    # renvoie le couple pour bézout et le pgcd
 
-    return u_k, v_k
 
+print(euclide_etendu(17,60))
+print(euclide_etendu(24,18))
+
+
+def euclide_simple(a,b):
+    if b == 0:
+        return a
+    else:
+        return euclide_simple(b, a%b)
+
+
+
+# euclide étendu récursif
+
+def euclet_rec(a,b):
+    if b == 0:
+        return (a, 1, 0)
+    else:
+        pgcd, u, v = euclet_rec(b, a%b)
+        u, v = v, u - (a//b) * v
+        return (pgcd, u, v)
